24f347e159c0c895f68bf0c9a49c92daec405bd6
Inês Mavioso <inesmavioso@gmail.com>
2022-10-02 12:02:37 +0100

    Do the same with the hour like i did with the minutes

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 let now = new Date();
 let currentDate = document.querySelector("#date");
 let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
 let minute = now.getMinutes();
 if (minute < 10) {
   minute = `0${minute}`;
