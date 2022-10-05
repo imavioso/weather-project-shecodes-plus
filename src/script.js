bd688e174e9d35aa929ed2e90c4bfd31b8d9b98a
Inês Mavioso <inesmavioso@gmail.com>
2022-10-05 19:15:23 +0100

    add api forecast and obtain latitude and longitude

--- src/script.js
+++ src/script.js
@@ -26,12 +26,12 @@ function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thursday", "Friday", "Saturday", "Sunday"];
+  let days = ["Thursday", "Friday", "Saturday", "Sunday", "Monday"];
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
       `
-    <div class="col-2">
+    <div class="col">
        <div class="weather-forecast-date">
           ${day}</div>
         <img
@@ -50,6 +50,14 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+//Forecast//
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "c6f8ef4575250284954db9f4dfa7a996";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 //Search Engine//
@@ -73,7 +81,11 @@ function displayWeather(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
+  //images//
 }
 
 function city(event) {
